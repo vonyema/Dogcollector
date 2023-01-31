7ac85377662cf476ea962e9e2f6ad34ed65ec3b3
Victoria Onyema <nolabby95@me.com>
2023-01-31 09:33:10 -0500

    works!

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'dogcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dogcollector',
     }
 }
